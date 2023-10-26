5ddeb3da9b59c7bdd48a176a650a65a97f1a2113
Wesam20061 <144031435+Wesam20061@users.noreply.github.com>
2023-10-26 11:39:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let getal = 0;
+let getalp = document.getElementById("Getal");
+
+function OptellenGetal() {
+    if (getal >= 10) {
+        getal = 0;
+    }
+    else {
+        getal++;
+    }
+    getalp.innerHTML = getal.toString();
+}
+
+
+
+
+
+
+
+
+
